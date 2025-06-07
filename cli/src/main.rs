64250d94b27fb540b9610a18c1432a63cbe1b2ca
Cassie Grey <cassgrey05@gmail.com>
2025-06-06 23:02:45 -0700

    some more improvements to handoutgen

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,55 @@
-use std::env;
+use std::{env, path::PathBuf};
+use clap::{Parser, Subcommand};
 
-use handoutgen::generate_document;
+#[derive(Subcommand)] 
+enum Commands {
+  Handoutgen {
+    #[clap(short = 'i', long, env)]
+    handout_file: PathBuf,
+
+    #[clap(short = 'o', long, env)]
+    out_file: PathBuf
+  },
+  Inithandout {
+    #[clap(short = 'o', long, env)]
+    out_file: PathBuf
+  }
+}
+
+#[derive(Parser)]
+struct CommandlineArgs {
+  #[command(subcommand)]
+  command: Commands
+}
+
+use handoutgen::{generate_document, init_handout};
 
 fn main() {
+  let commandline_args = CommandlineArgs::parse();
+
+  match commandline_args.command {
+    Commands::Handoutgen { handout_file, out_file } => {
+      match generate_document(handout_file, out_file) {
+        Err(e) => {
+          println!("Error generating document: {:?}", e);
+        },
+        Ok(()) => {
+          println!("Successfully generated document.");
+        }
+      }
+    },
+    Commands::Inithandout { out_file } => {
+      match init_handout(out_file) {
+        Err(e) => {
+          println!("Error creating handout: {:?}", e);
+        },
+        Ok(()) => {
+          println!("Successfully created handout.");
+        }
+      }
+    }
+  }
+
   let args: Vec<String> = env::args().collect();
 
   if args.len() < 2 {
@@ -12,13 +59,7 @@ fn main() {
   }
 
   if args[1] == "handoutgen" {
-    match generate_document() {
-      Err(e) => {
-        println!("Error generating document: {:?}", e);
-      },
-      Ok(()) => {
-        println!("Successfully generated document.");
-      }
-    }  
+
+      
   }
 }
