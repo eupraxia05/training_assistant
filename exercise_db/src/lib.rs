3c37576f0f1c7dd654ff814a44a18cf9669638df
Felis Draconus <>
2025-06-06 23:20:10 -0700

    add exercise struct, move db path to argument

--- exercise_db/src/lib.rs
+++ exercise_db/src/lib.rs
@@ -1,18 +1,20 @@
 use rusqlite::{Connection, Result};
+use std::path::{Path};
 
-// TODO: change path to some sane default value (possibly allow using a custom DB location later)
-pub fn init_db() -> Result<()> {
-    let path = "./exercise_db.db3";
-
-    // TODO: move this out of this function to allow re-use for other db calls (this is thread safe with rusqlite via compile time checks)
-    let conn = Connection::open(path)?;
+// TODO: consider making this a struct which stores (and returns) the db interface
+// TODO: consider adding support for a human readable format (json, etc)
+// TODO: spec out expected db interactions (search by name, add new exercise, get exercise list by name, get exercise list by id, etc)
+// TODO: add schema version value to table, plan for migrations from old formats (upgrading should never break custom/modified exercises)
+// TODO: add tests
+pub fn init_db(data_path: &Path) -> Result<()> {
+    let db_path = data_path.join("exercises_db.db3");
+    let conn = Connection::open(db_path)?;
 
     // transaction creates initial exercises table. additional tables (client/etc) should be added in this transaction
-    // TODO: only do this if tables do not yet exist
     // TODO: handle error
     conn.execute_batch(
         "BEGIN;
-        CREATE TABLE exercises(
+        CREATE TABLE IF NOT EXISTS exercises(
             id          INT PRIMARY KEY,
             name        TEXT NOT NULL,
             summary     TEXT,
@@ -20,4 +22,14 @@ pub fn init_db() -> Result<()> {
             image_paths TEXT);
         COMMIT;");
     Ok(())
-} 
+}
+
+// TODO: look at ways to detect at compile time (or at least test time) when this falls out of sync with db table format
+#[derive(Debug)]
+pub struct Exercise {
+    db_id: i32,
+    name: String,
+    summary: String,
+    steps: String,
+    image_paths: String
+}
